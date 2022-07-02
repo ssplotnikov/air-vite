09dc2615ce2e1d698cf81d8a040621127b0d29ad
SerezhaPlotnikov <frymeabeaver@gmail.com>
2022-07-02 11:23:45 +0300

    add react-hook-form

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { flightSlice } from './flight/flightSlice';
-
-export const store = configureStore({
-  reducer: {
-    flight: flightSlice.reducer,
-  },
-});
