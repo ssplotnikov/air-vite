ccded1c78723a4f1cd9e605d20cb6ff08e32a154
SerezhaPlotnikov <frymeabeaver@gmail.com>
2022-07-04 21:24:11 +0300

    create useInputNumbers

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import Flight from '../../components/flight/Flight';
 
 export default function Main() {
   return <Flight />;
 }
-=======
-import React from 'react';
-import { Flight } from '../../components/flight/Flight';
-
-export const Main = () => {
-  return <Flight />;
-};
->>>>>>> f1adbe04d3e95c8b38ac7259fecacec358058d6a
