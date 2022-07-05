cb38fc8d55442f4dcb56c696a161c9083d3f8d67
SerezhaPlotnikov <frymeabeaver@gmail.com>
2022-07-05 17:52:24 +0300

    merge for linux

---
+++ src/hooks/useInputNumbers.ts
@@ -0,0 +1,15 @@
+import { ChangeEvent, useState } from 'react'
+
+export const useInputNumbers = (initialValue: number | string = '') => {
+  const [value, setValue] = useState(initialValue)
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let finalValue: number
+    Number.isNaN(parseInt(e.target.value))
+      ? (finalValue = 0)
+      : (finalValue = parseInt(e.target.value))
+    console.log(value)
+
+    return setValue(finalValue)
+  }
+  return { value, onChange }
+}
